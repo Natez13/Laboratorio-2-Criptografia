63e993bc7262ce6223fc4fc2a2fd145c53ce6498
Natez <70248621+Natez13@users.noreply.github.com>
2022-06-05 23:36:33 -0400

    Add files via upload

--- Hito3/Hito3_CL/Driver.py
+++ Hito3/Hito3_CL/Driver.py
@@ -4,7 +4,10 @@ print("Test Execution Started")
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
-driver = webdriver.Remote(
-command_executor='http://localhost:4444/wd/hub',
-options=options)
-#driver = webdriver.Chrome(executable_path=r"/home/informatica/Downloads/chromedriver_linux64/chromedriver")
+#Usando Docker:
+"""
+#driver = webdriver.Remote(
+#command_executor='http://localhost:4444/wd/hub',
+#options=options)
+"""
+driver = webdriver.Chrome(executable_path=r"/chromedriver")
