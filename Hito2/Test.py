90ea6d580750929487a9af0f10e071002466cf06
Natez <70248621+Natez13@users.noreply.github.com>
2022-06-05 23:33:20 -0400

    Add files via upload

--- Hito2/Test.py
+++ Hito2/Test.py
@@ -1,17 +1,21 @@
 from re import I
-from selenium import webdriver
 import time
 import json
 from selenium.webdriver.common.by import By
+
+from selenium import webdriver
 
 print("Test Execution Started")
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
-driver = webdriver.Remote(
-command_executor='http://localhost:4444/wd/hub',
-options=options
-)
+#Usando Docker:
+"""
+#driver = webdriver.Remote(
+#command_executor='http://localhost:4444/wd/hub',
+#options=options)
+"""
+driver = webdriver.Chrome(executable_path=r"/chromedriver")
 
 with open("Data.json", "r") as read_file:
     data_json = json.load(read_file)
